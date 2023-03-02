c3980d69200bcc4951bd11af011601bf82c02982
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-02 12:17:37 -0300

    JavaScript introduction(TimeStamp)

--- Index.js
+++ Index.js
@@ -1,7 +1,2 @@
-window.addEventListener('focus', event => {
-    console.log("focus");
-});
-
-document.addEventListener ('click', event => {
-    console.log('click');
-});
+let agora = new Date();
+console.log(agora);
