6c1ed0cd416562d9b9f68834a163cd8b7e415430
Ricardo Lucena <ricardos.lucena99@gmail.com>
2023-03-02 10:03:45 -0300

    JavaScript introduction (comments and variables)

---
+++ Index.js
@@ -0,0 +1,12 @@
+var olamundo= "Hello World" 
+
+console.log(olamundo);
+console.log(olamundo);
+console.log(olamundo);
+console.log(olamundo);
+
+// Aqui eu consigo fazer um comentario na linha
+
+/* aqui eu consigo
+fazer um comentario
+em bloco*/
