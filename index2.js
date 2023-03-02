0549a7fb04413792304557a698e902bcc1c1f140
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-02 11:11:26 -0300

    JavaScript introduction(switch)

---
+++ index2.js
@@ -0,0 +1,12 @@
+        let cor = "azul";
+    switch (cor) {
+    case "verde":
+        console.log("siga");
+        break ;
+    case "amarelo":
+        console.log("pare");
+        break;
+        default:
+            console.log("não sei");
+
+    }
